5712bc89ff724de7c28d042c9af0ef3661372c22
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:03:51 -0300

    Update Lista2_15.py

--- Lista2_15.py
+++ Lista2_15.py
@@ -1,12 +1,10 @@
 '''
-Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
-  Média de Aproveitamento  Conceito
-  Entre 9.0 e 10.0        A
-  Entre 7.5 e 9.0         B
-  Entre 6.0 e 7.5         C
-  Entre 4.0 e 6.0         D
-  Entre 4.0 e zero        E
-O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+Faça um Programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+Dicas:
+Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+Triângulo Equilátero: três lados iguais;
+Triângulo Isósceles: quaisquer dois lados iguais;
+Triângulo Escaleno: três lados diferentes;
 '''
 
 lado1 = float(input("Lado 1: "))
