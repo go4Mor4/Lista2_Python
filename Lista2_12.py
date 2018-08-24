5303e6ef892afdcc7e99c016a8633bfd505eb63d
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 17:35:24 -0300

    Update Lista2_12.py

--- Lista2_12.py
+++ Lista2_12.py
@@ -1,3 +1,18 @@
+'''
+Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do Salário Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos os descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+Desconto do IR:
+Salário Bruto até 900 (inclusive) - isento
+Salário Bruto até 1500 (inclusive) - desconto de 5%
+Salário Bruto até 2500 (inclusive) - desconto de 10%
+Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, dispostas conforme o exemplo abaixo. No exemplo o valor da hora é 5 e a quantidade de hora é 220.
+        Salário Bruto: (5 * 220)        : R$ 1100,00
+        (-) IR (5%)                     : R$   55,00  
+        (-) INSS ( 10%)                 : R$  110,00
+        FGTS (11%)                      : R$  121,00
+        Total de descontos              : R$  165,00
+        Salário Liquido                 : R$  935,00
+'''
+
 valor_hora = float(input("Quanto você ganha por hora? "))
 horas_mes = int(input("Quantas horas você trabalha no mês? "))
 salario_bruto = horas_mes * valor_hora
