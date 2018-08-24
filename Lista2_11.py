f1840564d7d894c84716b1cdc3d8a47b502e1d12
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 17:34:52 -0300

    Update Lista2_11.py

--- Lista2_11.py
+++ Lista2_11.py
@@ -1,3 +1,16 @@
+'''
+As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver o programa que calculará os reajustes.
+Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+salários até R$ 280,00 (incluindo) : aumento de 20%
+salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+o salário antes do reajuste;
+o percentual de aumento aplicado;
+o valor do aumento;
+o novo salário, após o aumento.
+'''
+
 salario = float(input("Digite o seu salário"))
 
 valor_baixo = salario * 0.20
