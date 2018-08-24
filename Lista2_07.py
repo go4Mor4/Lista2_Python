e4b08b1be3c2df74e0743ece9fd7385405f81fdf
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 17:32:19 -0300

    Update Lista2_07.py

--- Lista2_07.py
+++ Lista2_07.py
@@ -1,3 +1,5 @@
+#Faça um Programa que leia três números e mostre o maior e o menor deles.
+
 valor1 = float(input("Digite o primeiro valor: "))
 valor2 = float(input("Digite o segundo valor: "))
 valor3 = float(input("Digite o terceiro valor: "))
