392cfad156337fa8baf55265b046027757e4e194
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:03:17 -0300

    Update Lista2_14.py

--- Lista2_14.py
+++ Lista2_14.py
@@ -1,4 +1,13 @@
-#Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+'''
+Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
+  Média de Aproveitamento  Conceito
+  Entre 9.0 e 10.0        A
+  Entre 7.5 e 9.0         B
+  Entre 6.0 e 7.5         C
+  Entre 4.0 e 6.0         D
+  Entre 4.0 e zero        E
+O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+'''
 
 nota1 = float(input("Digite a nota1"))
 nota2 = float(input("Digite a nota2"))
