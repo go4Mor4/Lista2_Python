e10190dfd7fe0e4141972c776276e313e2734fac
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:07:21 -0300

    Update Lista2_22.py

--- Lista2_22.py
+++ Lista2_22.py
@@ -1,3 +1,7 @@
+'''
+Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
+'''
+
 numero = int(input("Digite um número inteiro: "))
 
 if numero % 2 == 0:
