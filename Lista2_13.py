c3c9b830369813ee106773a309bfcebefc458e23
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 17:35:48 -0300

    Update Lista2_13.py

--- Lista2_13.py
+++ Lista2_13.py
@@ -1,3 +1,5 @@
+#Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+
 dia = int(input("Digite um número de 1 a 7: "))
 
 if dia == 1:
