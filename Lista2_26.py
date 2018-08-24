9c6a081491ab1fed2f5529d14bcd856becefe931
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:09:27 -0300

    Update Lista2_26.py

--- Lista2_26.py
+++ Lista2_26.py
@@ -1,3 +1,13 @@
+'''
+Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+Álcool:
+até 20 litros, desconto de 3% por litro
+acima de 20 litros, desconto de 5% por litro
+Gasolina:
+até 20 litros, desconto de 4% por litro
+acima de 20 litros, desconto de 6% por litro Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
+'''
+
 litros = int(input("Digite a quantidade de litros: "))
 combustivel = input("Digite o tipo do combustivel [G, A]: ")
 
