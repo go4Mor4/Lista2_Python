b3a6576d8c09d6073cf7f0159857919b1a673e7e
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:04:36 -0300

    Update Lista2_17.py

--- Lista2_17.py
+++ Lista2_17.py
@@ -1,3 +1,7 @@
+'''
+Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
+'''
+
 ano = int(input("Digite o ano"))
 
 if (ano%4 == 0 and ano%100 != 0) or ano%400 == 0:
