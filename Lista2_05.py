c32d2641991804d60d2b203edd21d2e87239a3a6
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 17:31:44 -0300

    Update Lista2_05.py

--- Lista2_05.py
+++ Lista2_05.py
@@ -1,3 +1,10 @@
+'''
+Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar:
+A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+A mensagem "Reprovado", se a média for menor do que sete;
+A mensagem "Aprovado com Distinção", se a média for igual a dez.
+'''
+
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 
