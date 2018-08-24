042a3cdb0216f5aad72fcae2b9e903b1f73e3647
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 17:31:17 -0300

    Update Lista2_04.py

--- Lista2_04.py
+++ Lista2_04.py
@@ -1,3 +1,5 @@
+#Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
 letra = input("Digite uma letra: ")
 
 if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
