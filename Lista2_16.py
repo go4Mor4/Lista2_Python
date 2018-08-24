a38769d39c51ef2e946db58b715b53f19e052d41
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:04:13 -0300

    Update Lista2_16.py

--- Lista2_16.py
+++ Lista2_16.py
@@ -1,3 +1,11 @@
+'''
+Faça um programa que calcule as raízes de uma equação do segundo grau, na forma ax2 + bx + c. O programa deverá pedir os valores de a, b e c e fazer as consistências, informando ao usuário nas seguintes situações:
+Se o usuário informar o valor de A igual a zero, a equação não é do segundo grau e o programa não deve fazer pedir os demais valores, sendo encerrado;
+Se o delta calculado for negativo, a equação não possui raizes reais. Informe ao usuário e encerre o programa;
+Se o delta calculado for igual a zero a equação possui apenas uma raiz real; informe-a ao usuário;
+Se o delta for positivo, a equação possui duas raiz reais; informe-as ao usuário;
+'''
+
 import math
 a = float(input("Digite A: "))
 if a == 0:
