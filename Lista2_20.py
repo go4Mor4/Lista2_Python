e9189326ccf36021ffbff48fdbe00eeea300936a
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:06:22 -0300

    Update Lista2_20.py

--- Lista2_20.py
+++ Lista2_20.py
@@ -1,3 +1,9 @@
+'''
+Faça um Programa para leitura de três notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e presentar:
+A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada;
+A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
+A mensagem "Aprovado com Distinção", se a média for igual a 10.
+'''
 # NA LISTA O EXERCÍCIO 5 E O 20 SÃO O MESMO
 
 nota1 = float(input("Digite a primeira nota: "))
