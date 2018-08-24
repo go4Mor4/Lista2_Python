d0cc0c71f3ec65095b9336f03287361ca35b0729
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:07:52 -0300

    Update Lista2_23.py

--- Lista2_23.py
+++ Lista2_23.py
@@ -1,3 +1,7 @@
+'''
+Faça um Programa que peça um número e informe se o número é inteiro ou decimal. Dica: utilize uma função de arredondamento.
+'''
+
 numero = float(input("Digite um número: "))
 
 if(numero // 1 == numero):
