f5a7249b000ffddc6a9d166f2fe16528cfb0d659
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:01:45 -0300

    Update Lista2_14.py

--- Lista2_14.py
+++ Lista2_14.py
@@ -1,3 +1,5 @@
+#Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+
 nota1 = float(input("Digite a nota1"))
 nota2 = float(input("Digite a nota2"))
 media = (nota1 + nota2) / 2
