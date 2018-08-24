1f2046ee3ce79f55a51d34610d57cdf80aba2cc8
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 20:08:20 -0300

    Update Lista2_24.py

--- Lista2_24.py
+++ Lista2_24.py
@@ -1,3 +1,10 @@
+'''
+Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar. O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
+par ou ímpar;
+positivo ou negativo;
+inteiro ou decimal.
+'''
+
 numero1 = float(input("Digite o número 1: "))
 numero2 = float(input("Digite o número 2: "))
 operacao = input("Digite a operação que deseja realizar: [+, -, /, *]: ")
