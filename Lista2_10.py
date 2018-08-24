e271d83db955b9baea0af50feb0b24faf94cd000
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 17:33:50 -0300

    Update Lista2_10.py

--- Lista2_10.py
+++ Lista2_10.py
@@ -1,3 +1,6 @@
+#Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+
 horario = input("Digite [M, V, N]: ")
 
 if horario == 'M' or horario == 'm':
