0f2c98562a4a6adfea606eed0ddea50e22e61479
Norman <42472359+NormanDeveloper@users.noreply.github.com>
2018-08-24 17:33:17 -0300

    Update Lista2_09.py

--- Lista2_09.py
+++ Lista2_09.py
@@ -1,3 +1,5 @@
+#Faça um Programa que leia três números e mostre-os em ordem decrescente.
+
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 n3 = float(input("Digite o terceiro número: "))
