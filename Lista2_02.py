cb5a8351014af3f2450dbb22fea9174d9c8258c6
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-25 19:19:29 -0300

    Update Lista2_02.py

--- Lista2_02.py
+++ Lista2_02.py
@@ -2,7 +2,5 @@
 
 valor = float(input("Digite um valor: "))
 
-if valor < 0:
-    print("O valor é negativo")
-else:
-    print("O valor é positivo")
+if valor < 0: print("O valor é negativo")
+else: print("O valor é positivo")
