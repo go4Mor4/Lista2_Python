5a26105222d5f516a549d37a2754961d77bba3f7
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-25 19:20:00 -0300

    Update Lista2_03.py

--- Lista2_03.py
+++ Lista2_03.py
@@ -2,9 +2,6 @@
 
 sexo = input("Digite seu sexo [F, M]: ")
 
-if sexo == 'F' or sexo == 'f':
-    print("Seu sexo é Feminino")
-elif sexo == 'M' or sexo == 'm':
-    print("Seu sexo é Masculino")
-else:
-    print("Sexo Inválido")
+if sexo == 'F' or sexo == 'f': print("Seu sexo é Feminino")
+elif sexo == 'M' or sexo == 'm': print("Seu sexo é Masculino")
+else: print("Sexo Inválido")
