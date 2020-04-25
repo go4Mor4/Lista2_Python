12fd90785855526698c5c1c50092abc04f10df62
Gabriel Mora <42472359+go4Mor4@users.noreply.github.com>
2020-04-25 19:18:55 -0300

    Update Lista2_01.py

--- Lista2_01.py
+++ Lista2_01.py
@@ -3,9 +3,6 @@
 n1 = float(input("Digite um número: "))
 n2 = float(input("Digite outro número: "))
 
-if n1 > n2:
-    print("O maior número é ", n1)
-elif n2 > n1:
-    print("O maior número é ", n2)
-else:
-    print("Os números são iguais")
+if n1 > n2: print("O maior número é ", n1)
+elif n2 > n1: print("O maior número é ", n2)
+else: print("Os números são iguais")
